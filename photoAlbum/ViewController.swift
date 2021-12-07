d9f356dae0a68083218593467c0bcdb8aa9b29c5
danielleebere <danielleebereanukem@gmail.com>
2021-12-07 18:38:15 +0000

    Added an add button, which allows me to add rows to the table; learnt I can't have add button in table view cell

--- photoAlbum/ViewController.swift
+++ photoAlbum/ViewController.swift
@@ -7,7 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+   
+    var albumNames = [""]
+    
+    @IBOutlet var table: UITableView!
+    
+    @IBAction func add(_ sender: Any) {
+        albumNames.insert("New", at: 0)
+        //albumNames.append("New")
+        
+        table.reloadData()
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("tapped")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return albumNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = albumNames[indexPath.row]
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
