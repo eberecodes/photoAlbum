ac077cc4ad2be2cf278def0ef563bcaa7f021318
danielleebere <danielleebereanukem@gmail.com>
2021-12-08 10:37:11 +0000

    Added in feature to name album using an alert pop-up; added disclosure indicator, you can navigate to new screens using a segue; added collection view for gallery

--- photoAlbum/ViewController.swift
+++ photoAlbum/ViewController.swift
@@ -4,25 +4,55 @@
 //
 //  Created by Ebere Anukem on 07/12/2021.
 //
+//TODO: Add a navigation controller
+//TODO: work on 'Example' album and use that to build out album (database design will need to be considered) - peristent storage
+//TODO: Add image to the side of table view (for locked albums make it an image of a lock) otherwise use previw from their album
+//TODO: add delete, edit and modify functionality
 
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
    
-    var albumNames = [""]
+    var albumNames = ["Example"] //can call this example or sample maybe
     
-    @IBOutlet var table: UITableView!
+    @IBOutlet weak var table: UITableView!
     
     @IBAction func add(_ sender: Any) {
-        albumNames.insert("New", at: 0)
+        //albumNames.insert("New", at: 0)
         //albumNames.append("New")
         
-        table.reloadData()
+        //1. Create the alert controller.
+        let alert = UIAlertController(title: "New Album", message: "Enter album name", preferredStyle: .alert)
+
+        //2. Add the text field. You can configure it however you need.
+        alert.addTextField { (textField) in
+            textField.text = ""
+            //potentially restrict so no empty field can be entered
+        }
+        
+        
+        // 3. Grab the value from the text field, and print it when the user clicks OK.
+        alert.addAction(UIAlertAction(title: "Save", style: .default, handler: { [weak alert] (_) in
+            let textField = alert!.textFields![0] // Force unwrapping because we know it exists.
+            print("Text field: \(textField.text ?? "new")")
+            self.albumNames.append(textField.text ?? "new")
+            self.table.reloadData()
+        }))
+
+        // 4. Present the alert.
+        self.present(alert, animated: true, completion: nil)
+        
+        
+        
+        
+        
+        //table.reloadData()
     }
     
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("tapped")
+        performSegue(withIdentifier: "toGallery", sender: nil)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -34,6 +64,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
         cell.textLabel?.text = albumNames[indexPath.row]
+        
+        cell.accessoryType = .disclosureIndicator
+        
         return cell
     }
     
---
+++ photoAlbum/galleryVC.swift
@@ -0,0 +1,35 @@
+//
+//  galleryVC.swift
+//  photoAlbum
+//
+//  Created by Ebere Anukem on 08/12/2021.
+//
+
+import UIKit
+
+class galleryVC: UIViewController {
+    
+    @IBOutlet weak var name: UILabel!
+    
+    
+    @IBOutlet weak var galleryCollection: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
