5d9d8cc1d08002383cf0c3bdb10148ab16ed1f69
danielleebere <danielleebereanukem@gmail.com>
2022-03-20 11:24:08 +0000

    code clean

--- photoAlbum/Album/Album.swift
+++ photoAlbum/Album/Album.swift
@@ -2,8 +2,7 @@
 //  Album.swift
 //  photoAlbum
 //
-//  Created by Ebere Anukem on 11/12/2021.
-//
+
 
 import CoreData
 
@@ -12,7 +11,6 @@ import CoreData
 class Album: NSManagedObject{
     @NSManaged var id: NSNumber!
     @NSManaged var title: String!
-    @NSManaged var deletedDate: Date? //I can remove this
     @NSManaged var photoGallery: Data?
     @NSManaged var lockStatus: String!
 }
